672574629eed64e008d8b0798b89a998ac864a9e
gautier <gautier.vaillant@epitech.eu>
2020-10-16 14:11:01 +0200

    api works, algo to finish

--- src/App.js
+++ src/App.js
@@ -8,19 +8,34 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { input: "" };
+    this.state = {
+      input: "",
+      token: 'Q5j2hPCD765Hrz6k',
+      userid: '8173'
+    };
   }
 
   saveInput(newInput) {
     this.setState({ input: newInput });
-    this.synonym()
   }
 
   synonym() {
+    console.log(this.state);
     var wordTab = this.state.input.split(' ');
     var requetesTab = [];
-    wordTab.forEach(current => requetesTab.push(Axios.get("https://wordsapiv1.p.mashape.com/words/" + current + "/synonyms")));
-    Axios.all(requetesTab.map(function(current){return current})).then(function(info) {console.log(info.map)})
+    var synonymTabs = [];
+    wordTab.forEach(current => requetesTab.push(Axios.get("https://www.abbreviations.com/services/v2/syno.php?uid=" + this.state.userid + "&tokenid=" + this.state.token + "&word=" + current + "&format=json")));
+    Axios.all(requetesTab).then(Axios.spread((...responses) => {
+      console.log(responses);
+      for (let i = 0; i < wordTab.length; i++) {
+        console.log("i = " + i)
+        let allSynonyms = responses[i]['data']['result'][0]['synonyms'];
+        console.log("synonyms = '" + allSynonyms + "'");
+      }
+      // use/access the results 
+    })).catch(errors => {
+      // react on errors.
+    })
   }
 
 
@@ -32,6 +47,7 @@ class App extends Component {
         Yes, this is completely useless. A bit funny, though.</p>
         <Saisie
           saveInput={this.saveInput.bind(this)} />
+        <button onClick={this.synonym.bind(this)}>Synonymize</button>
         <Resultat
           text={this.state.input} />
       </div>
